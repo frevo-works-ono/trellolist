bccd860e57ccfb10cbb9c8799e7256b28585d322
h.ono <h.ono@frevo-works.co.jp>
2017-09-01 17:59:24 +0900

    initial commit

--- src/vuex/modules/common.js
+++ src/vuex/modules/common.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export const webClient = axios.create({
   params: {
-    key: '87fe0aaff3df4d3b485cc83c1a691426',
-    token: '4390222300e5208c94d4a6832ecc5203e1bdfd5cf3e04f54fe67bd8c3b3aaf6c'
+    key: '<API Key>',
+    token: '<Token>'
   }
 })
